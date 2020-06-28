74339d5f29807ece25d906a677b1d0a8e3745d5e
Tanaka Yukio <news7vip@gmail.com>
2020-06-28 14:01:06 +0900

    revise bugs

--- src/ZerochSharp/Controllers/BoardsSSRController.cs
+++ src/ZerochSharp/Controllers/BoardsSSRController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Text;
 using System.Threading.Tasks;
 using ZerochSharp.Models;
+using ZerochSharp.Models.Boards;
 
 // ReSharper disable once EmptyNamespace
 namespace ZerochSharp.Controllers
